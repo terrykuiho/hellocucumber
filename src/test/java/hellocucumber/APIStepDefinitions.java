86249efae6149fc61714c1a3472a0fc66ae19748
terrykuiho <terrykuiho@gmail.com>
2023-01-04 13:53:18 -0800

    First Commit

---
+++ src/test/java/hellocucumber/APIStepDefinitions.java
@@ -0,0 +1,60 @@
+package hellocucumber;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class APIStepDefinitions {
+
+    @Given("Customer get API is provided")
+    public void customer_get_api_is_provided() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("User call customer get API")
+    public void user_call_customer_get_api() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Specific customer info will be shown")
+    public void specific_customer_info_will_be_shown() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("Customer list API is provided")
+    public void customer_list_api_is_provided() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("User call customer list API")
+    public void user_call_customer_list_api() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("Customer list will be shown")
+    public void customer_list_will_be_shown() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("Login API is provided")
+    public void login_api_is_provided() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("User call login API")
+    public void user_call_login_api() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("a token will be generated")
+    public void a_token_will_be_generated() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
+
---
+++ src/test/java/hellocucumber/StepDefinitions.java
@@ -0,0 +1,53 @@
+package hellocucumber;
+
+import io.cucumber.java.en.*;
+
+import org.junit.jupiter.api.Assertions.*;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+
+import java.util.Locale;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StepDefinitions {
+
+    private String today;
+    private String actualAnswer;
+
+    @Given("today is Sunday")
+    public void today_is_sunday() {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+        today = "Sunday";
+    }
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_friday_yet() {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+        actualAnswer = IsItFriday.isItFriday(today);
+    }
+    @Then("I should be told {string}")
+    public void i_should_be_told(String expectedAnswer) {
+        // Write code here that turns the phrase above into concrete actions
+        //throw new io.cucumber.java.PendingException();
+        assertEquals(expectedAnswer, actualAnswer);
+    }
+
+    @Given("today is Friday")
+    public void today_is_Friday() {
+        today = "Friday";
+    }
+}
+class IsItFriday {
+    static String isItFriday(String today) {
+        String answer = "Nope";
+        if (today.equals("Friday")) {
+            answer = "TGIF";
+        }
+        return answer;
+    }
+}
+
